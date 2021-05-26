0293b587e046b3a6a7680e061e833e354aac0677
ONDR10 <79906689+ONDR10@users.noreply.github.com>
2021-05-26 17:23:31 +0200

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-let pozicion = 0;
-let y = 0;
-let i = 0;
-
+let i = 0
+let y = 0
+let pozicion = 0
 let level = 1
+let tah = 0;
 function incraselevel(myint = 0)
 {
     if (myint != 3) {
         myint++;
     }
     else {
-        myint = 0;
+        myint = 1;
     }
     return myint;
 }
@@ -24,24 +24,22 @@ function decraselevel(myint = 0)
     return myint;
 }
 basic.forever(function () {
-    serial.writeString("Vyber si uroven (1 - 3): ")
+    basic.showString("Vyber si obtiaznost (1-3): ")
     while (1) {
-        basic.showNumber(level);
+        basic.showNumber(level)
         input.onButtonPressed(Button.A, function () {
             level = decraselevel(level);
         })
-            input.onButtonPressed(Button.B, function () {
+        input.onButtonPressed(Button.B, function () {
             level = incraselevel(level);
         })
-            input.onButtonPressed(Button.AB, function () {
+        input.onButtonPressed(Button.AB, function () {
             i = 1;
         })
-        if (i == 1) {
+    if (i == 1) {
             break;
         }
     }
-
-
     basic.clearScreen()
     for (i = 0; i < 5; i++) {
         for (y = 0; y < 5; y++) {
@@ -51,6 +49,35 @@ basic.forever(function () {
         }
     }
     pause(10000);
+    let pocet_tahov = 0;
+    while (1) {
+        // tah hraca
+        if (tah == 0) {
+            ++pocet_tahov;
+            
+            tah = 1;
+        }
+        //tah bota 
+        else {
+            pocet_tahov++;
+            if (level == 1) {
+
+            }
+            else if (level == 2) {
+
+            }
+            else {
+
+            }
+            tah = 0;
+        }
+        // vyhodnotenie
+        if (1) {
+
+        }
+        if (pocet_tahov == 9) {
+            basic.showString("Draw!");
+            break;
+        }    
+    }
 })
-
-
